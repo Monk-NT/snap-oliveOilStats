e0e17cece90a409dd6f6d2f874c825dd5ff9b7cb
Monk-NT <monk.nt@gmail.com>
2013-01-08 21:56:29 +0100

    HLint suggestions, and some changes to select function

--- src/Data/Analytics.hs
+++ src/Data/Analytics.hs
@@ -10,10 +10,10 @@ average xs = foldr (+) 0.0 xs / realToFrac (length xs)
 
 median :: (Fractional a, Ord a) => [a] -> a
 median xs
- | odd  $ length xs = select xs (length xs `div` 2)
- | even $ length xs = average  $ [(select xs k)] ++ [(select xs (k-1))]
+ | odd  $ length xs = select xs k
+ | even $ length xs = average  $ select xs k : [select xs (k-1)]
  where k = length xs `div` 2
 
 
 select :: (Ord a) => [a] -> Int -> a 
-select xs k = (!!) (sort xs) (k-1)  
+select xs k = (!!) (sort xs) k  
